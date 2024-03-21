a38d90d19f0f41958d938acc70cd6a3f71620add
Marko Korkiakoski <marko.korkiakoski@hiddentrail.com>
2024-03-21 13:10:16 +0200

    Added target software with a seldom occurring problem

---
+++ target_software/seldom_occurring_error.py
@@ -0,0 +1,44 @@
+import datetime
+import logging
+import sys
+import time
+
+logging.basicConfig(filename="luckynumber.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def lucky_number(meaningless_parameter=0):
+
+    luckynumber = None
+    if meaningless_parameter:
+        logging.debug(f"sleeping {meaningless_parameter} ms")
+        time.sleep(meaningless_parameter/1000)
+    try:
+        thousands = datetime.datetime.now().microsecond // 1000
+        parts_million = abs(1000000 // (thousands-13))
+        luckynumber = sum(int(digit) for digit in str(parts_million))
+        logging.debug(f"timestamp thousand: {thousands}, parts million: {parts_million}, lucky number: {luckynumber}")
+    except Exception as e:
+        logging.debug(f"error happened: {e}")
+
+    return luckynumber
+
+
+def generate_lucky_numbers(price, meaningless_parameter=0):
+
+    lns = {}
+    for _ in range(price):
+        ln = lucky_number(meaningless_parameter)
+        lns[ln] = ln
+
+    return sorted(sorted(lns))
+
+
+def main():
+
+    price = min(1000, int(sys.argv[1])) if len(sys.argv) > 1 else 1
+    another_parameter = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+
+    lucky_numbers = generate_lucky_numbers(price, another_parameter)
+    print(f"Your lucky numbers are: {lucky_numbers}")
+
+
+main()
