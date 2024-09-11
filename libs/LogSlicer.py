f3fd3898088be2b8ba6fbdb9b0b7fd3fd8e31f69
Marko Korkiakoski <marko.korkiakoski@hiddentrail.com>
2024-09-11 09:57:22 +0300

    Added requests import

--- libs/LogSlicer.py
+++ libs/LogSlicer.py
@@ -24,11 +24,7 @@ class LogSlicer:
         headers = {"Range": f"bytes={self.last_byte_position[url]}-"}
         response = requests.get(url, headers=headers)
         new_data = response.content.decode("utf-8")
-
-        # print("NEW DATA", new_data)
-
         self.process_new_log_lines(url, new_data)
-
         self.data[url] += new_data
         self.last_byte_position[url] += len(new_data)
 
@@ -45,9 +41,9 @@ class LogSlicer:
                     prev_epoch = epoch
 
                 self.log_lines[url].append((prev_epoch, matched_lines))
-                print("START--------------------------------------------------------------------------------------------------------")
-                print(f"{prev_epoch} -> {matched_lines}")
-                print("END----------------------------------------------------------------------------------------------------------\n")
+                # print("START--------------------------------------------------------------------------------------------------------")
+                # print(f"{prev_epoch} -> {matched_lines}")
+                # print("END----------------------------------------------------------------------------------------------------------\n")
                 prev_epoch = epoch
                 matched_lines = line
             else:
@@ -68,17 +64,38 @@ class LogSlicer:
 
         return epoch
 
-    def get_data(self, url):
-
-        self._refresh(url)
-
-        return self.data[url]
-
     def get_log_lines(self, url, start_ts=None, end_ts=None):
 
         self._refresh(url)
 
-        return self.log_lines[url]
+        if not start_ts and not end_ts:
+            log_lines = self.log_lines[url]
+        else:
+            if not start_ts:
+                start_ts = -62135516389.0  # == 02.01.0001 (smallest date strptime handles)
+            if not end_ts:
+                end_ts = 253402207200.0 # == 31.12.9999 (should be enough)
+
+            log_lines = []
+            for key, value in self.log_lines[url]:
+                # print(f"Key: {key}, Value: {value}")
+                print(f"{start_ts} <= {key} <= {end_ts}  =>  {start_ts <= key <= end_ts}")
+                if start_ts <= key <= end_ts:
+                    log_lines.append(value)
+
+        return log_lines
+
+    def get_log_lines_as_text(self, url, start_ts=None, end_ts=None):
+
+        log_lines = self.get_log_lines(url, start_ts, end_ts)
+
+        as_text = ""
+        for _, line in self.log_lines[url]:
+            if as_text:
+                as_text += "\n"
+            as_text += line
+
+        return as_text
 
 
 def _load_as_json(filename):
